a5eae5c8750d02587fa049d008ff2ef5d20e08af
Aeiddius <adriantagayom@gmail.com>
2025-05-22 16:58:39 +0800

    test

--- Modifier - All Attached Detail Group.py
+++ Modifier - All Attached Detail Group.py
@@ -6,8 +6,8 @@ from collections.abc import Callable
 
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.DB import ElementId, UV, ViewPlan, \
-                              Element, CurveArray, SketchPlane, XYZ, \
-                              FilteredElementCollector, BuiltInCategory
+    Element, CurveArray, SketchPlane, XYZ, \
+    FilteredElementCollector, BuiltInCategory
 
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
@@ -22,7 +22,7 @@ clr.AddReference("RevitServices")
 output = StringIO()
 sys.stdout = output
 
-# Scrict setup 
+# Scrict setup
 doc = DocumentManager.Instance.CurrentDBDocument
 active_view = doc.ActiveView
 
@@ -32,44 +32,49 @@ def transaction(func):
         TransactionManager.Instance.EnsureInTransaction(doc)
         func(*args, **kwargs)
         TransactionManager.Instance.TransactionTaskDone()
-    return wrapper 
+    return wrapper
+
 
 class Transact:
     def __init__(self):
         TransactionManager.Instance.EnsureInTransaction(doc)
+
     def __enter__(self):
         # print("Starting Transaction")
         return self
+
     def __exit__(self, exc_type, exc_value, traceback):
         # print("Ending Transaction")
         TransactionManager.Instance.TransactionTaskDone()
 
+
 # importing lib
-activate = IN[1] # type: ignore
-for script in IN[0]: # type: ignore
+activate = IN[1]  # type: ignore
+for script in IN[0]:  # type: ignore
     exec(script)
 
 # Functions and matrix definition
 matrix_a: dict[str, any] = locals().get("matrix_a")
 matrix_b: dict[str, any] = locals().get("matrix_b")
 print_member: Callable[[any], None] = globals().get("print_member")
-get_element  = globals().get("get_element")
+get_element = globals().get("get_element")
 get_elements = globals().get("get_elements")
 get_element_via_parameter = globals().get("get_element_via_parameter")
 get_view_range = globals().get("get_view_range")
 get_num: Callable[[str], int] = globals().get("get_num")
 get_parameter: Callable[[Element, str], str] = globals().get("get_parameter")
-set_parameter: Callable[[Element, str, any], bool] = globals().get("set_parameter")
+set_parameter: Callable[[Element, str, any],
+                        bool] = globals().get("set_parameter")
 is_dependent: Callable[[ViewPlan], bool] = globals().get("is_dependent")
 is_category_this = globals().get("is_category_this")
 collect_elements = globals().get("collect_elements")
 
-#==== Template ends here ====# 
+# ==== Template ends here ====#
 
 # This script is intended to generate unit sheet views
 
 
-# Parameters 
+# Parameters
 
 matrix = {
     "A": matrix_a,
@@ -78,29 +83,33 @@ matrix = {
 
 
 def get_dependent_views(target_group, target_subgroup, target_type):
-    views: List[ViewPlan] = get_view_range(target_group, target_subgroup, target_type)
+    views: List[ViewPlan] = get_view_range(
+        target_group, target_subgroup, target_type)
     units = {}
     for view in views:
         dependent_ids = view.GetDependentViewIds()
-        if not dependent_ids: continue
+        if not dependent_ids:
+            continue
         num = get_num(view.Name)
         units[num] = dependent_ids
     return units
- 
+
+
 def viewname_get_unit_type(view_name):
     if "ADA" in view_name:
         x = " ".join(view_name.replace("ADA", "").split(" ")[2:3])
         return x + " ADA"
     return view_name.split(" ")[-1].rsplit("-", 1)[0]
 
-def get_unit_type(view_name,TOWER):
+
+def get_unit_type(view_name, TOWER):
     view_unit_name = view_name.split(" ", 1)[-1].rsplit('-', 1)[0][2:].strip()
     print("?>???>>  ", view_unit_name)
     set_type = "x"
     for unit_name in matrix[TOWER]:
         # print(unit_name)
         unit = matrix[TOWER][unit_name]
-        
+
         if unit_name in view_unit_name:
             set_type = unit.ortn
             print("TYPE: ", unit_name)
@@ -113,73 +122,57 @@ def get_unit_type(view_name,TOWER):
                     return set_type
     if set_type == "x":
         print("NO UNIT FOR: ", view_unit_name)
-        raise Exception(f"No type for view [{view_name}] set in matrix") 
+        raise Exception(f"No type for view [{view_name}] set in matrix")
 
 
-# Body  
-@transaction      
-def start(): 
+# Body
+@transaction
+def start():
     target_group = "2. Presentation Views"
     target_subgroup = "b. Tower A"
     target_type = "Enlarged Rough-Ins"
-    
+
     target_units = get_view_range(target_group, target_subgroup, target_type)
     for view in target_units:
         groups = collect_elements(view, [BuiltInCategory.OST_IOSModelGroups])
-        ids = list(group.GetShownAttachedDetailGroupTypeIds(primary_view))
+
+        # Groups inside view
+        for group in groups:
+            reference_level = get_num(get_parameter(group, "Reference Level"))
+            if reference_level != lvl:
+                continue
+            if unit_Type_1 not in group.Name:
+                continue
+            ids = list(group.GetShownAttachedDetailGroupTypeIds(primary_view))
+            if ids:
+                continue
+
+            attached_detail = list(
+                group.GetAvailableAttachedDetailGroupTypeIds())
+            if len(attached_detail) == 1:
+                group.ShowAttachedDetailGroups(
+                    primary_view, attached_detail[0])
+                continue
+            else:
+                attached = False
+                for detail in attached_detail:
+                    detail_elem = get_element(detail)
+                    detail_name = get_parameter(detail_elem, "Type Name")
+
+                    if set_type in detail_name:
+                        with Transact():
+                            group.ShowAttachedDetailGroups(
+                                primary_view, detail_elem.Id)
+                            attached = True
+                            continue
+
+                    # Disposal
+                    detail_elem.Dispose()
+                    del detail_name
+                if not attached:
+                    raise Exception(f"No detail attached to {group.Name}")
 
 
-        print(lvl.Name)
-
-        continue
-        primary_view = ""   
-        print(lvl)
-        # Unit
-        for view_id in target_units[lvl]:
-            unit_view = get_element(view_id)
-            print(unit_view.Name)
-            unit_type = viewname_get_unit_type(unit_view.Name)
-            unit_Type_1 = f"(Type {unit_type})" 
-
-            # Check if primary id added
-            if not primary_view:
-                primary_view = get_element(unit_view.GetPrimaryViewId())
-            groups = collect_elements(unit_view, [BuiltInCategory.OST_IOSModelGroups])
-            
-            set_type = get_unit_type(unit_view.Name, "A")
-
-
-            # Groups inside view
-            for group in groups:
-                reference_level = get_num(get_parameter(group, "Reference Level"))
-                if reference_level != lvl: continue
-                if unit_Type_1 not in group.Name: continue
-                ids = list(group.GetShownAttachedDetailGroupTypeIds(primary_view))
-                if ids: continue
-
-                attached_detail = list(group.GetAvailableAttachedDetailGroupTypeIds())
-                if len(attached_detail) == 1:
-                    group.ShowAttachedDetailGroups(primary_view, attached_detail[0])
-                    continue
-                else: 
-                    attached = False
-                    for detail in attached_detail:
-                        detail_elem = get_element(detail)
-                        detail_name = get_parameter(detail_elem, "Type Name")
-
-                        if set_type in detail_name:
-                            with Transact():
-                                group.ShowAttachedDetailGroups(primary_view, detail_elem.Id)
-                                attached = True
-                                continue 
-
-                        # Disposal
-                        detail_elem.Dispose() 
-                        del detail_name
-                    if not attached:
-                        raise Exception(f"No detail attached to {group.Name}")
-
- 
-if activate:     
-    start()    
-OUT = output.getvalue()     
+if activate:
+    start()
+OUT = output.getvalue()
